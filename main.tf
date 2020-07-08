ee7d235c11d21704eb516762731b7b86a2544098
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-07-08 14:02:57 -0400

    upgraded to microservices 2.0.0 which includes istio and gremlin

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 // Modules
 module "microservices" {
   source  = "app.terraform.io/koinonia/microservices/aws"
-  version = "1.0.0"
+  version = "2.0.0"
 
   eks_cluster_name = "${var.eks_cluster_name}"
   region = "${var.region}"
@@ -14,4 +14,6 @@ module "microservices" {
   vpc1_public_subnet_cidr = "${var.public_subnet_cidr}"
   vpc1_vpc_name = "${var.vpc_name}"
   vpc1_worker_subnet_cidr = "${var.worker_subnet_cidr}"
+  gremlin_secret_teamID = "${var.gremlin_secret_teamID}"
+  gremlin_secret_teamSecret = "${var.gremlin_secret_teamSecret}" 
 }
